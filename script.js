5a7c0ed70ac7fc9dc6d477a7508d195ef685ffcf
User <user@Users-MacBook-Pro.local>
2020-10-13 13:07:12 -0400

    edit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Engineer", "Designer", "Freelancer"],
+        strings: ["a Developer", "an Engineer", "a Designer", "a Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Engineer", "Designer", "Freelancer", "Developer"],
+        strings: ["an Engineer", "a Designer", "a Freelancer", "a Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
